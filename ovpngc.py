703cf3677c791d163936b81a384e0330947e86fe
Tomka Gergely <tomkatudor@gmail.com>
2019-12-09 16:17:04 +0100

    Added verbose and autologin option, but unable to test it.

--- ovpngc.py
+++ ovpngc.py
@@ -33,17 +33,31 @@ parser.add_argument(
 parser.add_argument(
     '-U', '--url', help='The OpenVPN portal hostname, protocol and port')
 parser.add_argument(
+    '-v', '--verbose', help='Dump http content', action = 'store_true')
+parser.add_argument(
+    '-a', '--autologin', help='''Try to get an autologin configuration. 
+    This may not work and trying it may be a breach of corporate 
+    policies.''', action = 'store_true')
+parser.add_argument(
     '-f', '--file', help='The file to write the resulting configuration',
     default='client.ovpn')
 args = parser.parse_args()
 
 url = args.url + 'rest/GetUserlogin'
+print(args)
+if args.autologin :
+    if args.verbose:
+        print('Your warranty may be void, trying to get autologin config.')
+    url = args.url + 'rest/GetAutologin'
 
 if args.password is None:
     args.password = getpass.getpass()
 
 print('First query...')
 text = geturl(url, args.user, args.password)
+if args.verbose:
+    print(url)
+    print(text)
 secret = []
 for line in text.split('\n'):
     if 'CRV1:R,E' in line:
@@ -51,6 +65,9 @@ for line in text.split('\n'):
 number = getpass.getpass('Google number please: ')
 pw = '::'.join(('CRV1', secret[2], number))
 print('Second query...')
+if args.verbose:
+    print(pw)
+    print(text)
 text = geturl(url, args.user, pw)
 print('Writing out results to the file ', args.file)
 with open(args.file, 'w') as f:
